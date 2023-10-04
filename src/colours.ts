4566830290c3d5683b8468c1cd869d2ddaa1892e
SabeloX <sabelomtw@gmail.com>
2023-10-04 18:05:26 +0200

    Removed tailwindcss and added bulma

---
+++ src/colours.ts
@@ -0,0 +1,10 @@
+export const Colours = {
+    primaryMain: "#00b1d2",
+    primaryActive: "#00b89c",
+    primaryDark: "#00947E",
+    textWhite: "#ffffff",
+    textWhite100: "#fafafa",
+    textWhite200: "#f5f5f5",
+    textBlack: "#000000",
+    textBlack700: "#242424",
+}
