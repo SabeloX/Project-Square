70d78f412edd9028bc089713e5df680c563d2bb2
SabeloX <sabelomtw@gmail.com>
2023-10-04 18:44:51 +0200

    Saved colors in the global.css file as variables

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+    return (
+        <nav className={`navbar ${styles.navContainer}`}>
+            <div className="navbar-brand">
+                <div className="navbar-item">sa</div>
+            </div>
+        </nav>
+    )
+}
